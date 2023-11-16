0313106cb3f6f072e0ceb822972634f96cf5d1c7
Veer Kunwer Bharti <veer7549kr@gmail.com>
2023-11-17 01:57:45 +0530

    Implemented mouse draggable slides

--- public/js/script.js
+++ public/js/script.js
@@ -2,12 +2,16 @@ const slider = document.querySelector(".slider");
 const firstSlideWidth = slider.querySelector(".slide").offsetWidth;
 
 document.addEventListener("DOMContentLoaded", function () {
+  let isDragging = false,
+    startX,
+    startScrollLeft;
+
   function slidePrev() {
-    slider.scrollLeft += firstSlideWidth;
+    slider.scrollLeft += firstSlideWidth + 16;
   }
 
   function slideNext() {
-    slider.scrollLeft -= firstSlideWidth;
+    slider.scrollLeft -= firstSlideWidth + 16;
   }
 
   const checkKey = (e) => {
@@ -18,8 +22,29 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   };
 
+  const dragStart = (e) => {
+    isDragging = true;
+    slider.classList.add("dragging");
+    startX = e.pageX;
+    startScrollLeft = slider.scrollLeft;
+  };
+
+  const dragging = (e) => {
+    if (!isDragging) return;
+    slider.scrollLeft = startScrollLeft - (e.pageX - startX);
+  };
+
+  const dragStop = () => {
+    isDragging = false;
+    slider.classList.remove("dragging");
+  };
+
   document.querySelector("#prev-button").addEventListener("click", slidePrev);
   document.querySelector("#next-button").addEventListener("click", slideNext);
 
   document.onkeydown = checkKey;
+
+  slider.addEventListener("mousedown", dragStart);
+  slider.addEventListener("mousemove", dragging);
+  document.addEventListener("mouseup", dragStop);
 });
