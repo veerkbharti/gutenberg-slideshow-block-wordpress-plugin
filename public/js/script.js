36169b38d049fdc4489ba62d01bdf82ff0476c45
Veer Kunwer Bharti <veer7549kr@gmail.com>
2023-11-17 02:19:16 +0530

    Implemented infine scroll effects

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,28 @@
-const slider = document.querySelector(".slider");
-const firstSlideWidth = slider.querySelector(".slide").offsetWidth;
-
 document.addEventListener("DOMContentLoaded", function () {
+  const slider = document.querySelector(".slider");
+  const firstSlideWidth = slider.querySelector(".slide").offsetWidth;
+  const allSlides = [...document.querySelectorAll(".slide")];
+
   let isDragging = false,
     startX,
     startScrollLeft;
+
+  let slidePerView = Math.round(slider.offsetWidth / firstSlideWidth);
+
+  allSlides
+    .slice(-slidePerView)
+    .reverse()
+    .forEach((slide) => {
+      slider.insertAdjacentHTML("afterbegin", slide.outerHTML);
+    });
+
+  allSlides.slice(0, slidePerView).forEach((slide) => {
+    slider.insertAdjacentHTML("beforeend", slide.outerHTML);
+  });
+
+  slider.classList.add("no-transition");
+  slider.scrollLeft = slider.offsetWidth;
+  slider.classList.remove("no-transition");
 
   function slidePrev() {
     slider.scrollLeft += firstSlideWidth + 16;
@@ -39,6 +57,21 @@ document.addEventListener("DOMContentLoaded", function () {
     slider.classList.remove("dragging");
   };
 
+  const infiniteScroll = () => {
+    if (slider.scrollLeft === 0) {
+      slider.classList.add("no-transition");
+      slider.scrollLeft = slider.scrollWidth - 2 * slider.offsetWidth;
+      slider.classList.remove("no-transition");
+    } else if (
+      Math.ceil(slider.scrollLeft) ===
+      slider.scrollWidth - slider.offsetWidth
+    ) {
+      slider.classList.add("no-transition");
+      slider.scrollLeft = slider.offsetWidth;
+      slider.classList.remove("no-transition");
+    }
+  };
+
   document.querySelector("#prev-button").addEventListener("click", slidePrev);
   document.querySelector("#next-button").addEventListener("click", slideNext);
 
@@ -47,4 +80,6 @@ document.addEventListener("DOMContentLoaded", function () {
   slider.addEventListener("mousedown", dragStart);
   slider.addEventListener("mousemove", dragging);
   document.addEventListener("mouseup", dragStop);
+
+  slider.addEventListener("scroll", infiniteScroll);
 });
